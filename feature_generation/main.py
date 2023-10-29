b795bf4bf9504a021f548c1f721ead2a3d06c14d
NiceHit <silv20051@gmail.com>
2023-10-29 18:01:11 +0300

    E commit

--- feature_generation/main.py
+++ feature_generation/main.py
@@ -1,1 +1,26 @@
-print("Hello!")
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.covariance import empirical_covariance
+import numpy as np
+
+class FeatureGenerationTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, x_set, y_set=None):
+        self.cov_mat = empirical_covariance(x_set)
+        print(self.cov_mat)
+        return self
+
+    def transform(self, x_set):
+        return x_set
+
+
+if __name__ == '__main__':
+    x_set = np.array([
+        [1, 1, 1],
+        [0, 1, 0],
+        [2, 1, 2],
+        [7, 1, 7]
+    ])
+    feat_gen = FeatureGenerationTransformer()
+    feat_gen.fit(x_set)
