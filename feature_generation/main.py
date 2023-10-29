36b618abc5a37ce2bd25188da6ebd634a2bae440
NiceHit <silv20051@gmail.com>
2023-10-29 18:21:00 +0300

    E commit

--- feature_generation/main.py
+++ feature_generation/main.py
@@ -2,13 +2,18 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.covariance import empirical_covariance
 import numpy as np
 
+EPS = 1e-20
+
 class FeatureGenerationTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
 
     def fit(self, x_set, y_set=None):
         self.cov_mat = empirical_covariance(x_set)
-        print(self.cov_mat)
+        self.std_arr = np.std(x_set, axis=0).reshape((1,-1))
+        self.std_mat = self.std_arr.T @ self.std_arr
+        self.corr_mat = self.cov_mat / (self.std_mat + EPS)
+        
         return self
 
     def transform(self, x_set):
@@ -17,10 +22,10 @@ class FeatureGenerationTransformer(BaseEstimator, TransformerMixin):
 
 if __name__ == '__main__':
     x_set = np.array([
-        [1, 1, 1],
-        [0, 1, 0],
-        [2, 1, 2],
-        [7, 1, 7]
+        [1, 1, -1],
+        [0, 13, 0],
+        [2, 15, -2],
+        [7, 16, -7]
     ])
     feat_gen = FeatureGenerationTransformer()
     feat_gen.fit(x_set)
