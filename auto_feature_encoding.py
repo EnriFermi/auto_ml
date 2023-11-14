4ac4e16acb15c2d197b16ef8c39a8bc305dcdf49
Dmitry Karatyshchev <dmitrykaratyshchev@gmail.com>
2023-11-14 16:05:01 +0300

    Dmitry: Simple versions of Encoder and Normalizer are finished

--- auto_feature_encoding.py
+++ auto_feature_encoding.py
@@ -1,7 +1,6 @@
-from numpy import ndarray
 from sklearn.datasets import load_breast_cancer
-from IPython.display import display 
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
 import pandas as pd
@@ -9,30 +8,46 @@ import pandas as pd
 
 class Encoder(BaseEstimator, TransformerMixin):
     def __init__(self, categ_rate=0.2):
-        self.encoders = {'one_hot': OneHotEncoder}
+        self._encoders = {'one_hot': OneHotEncoder, 'label': LabelEncoder}
         self.category_rate = categ_rate
         self.categ_features_inds = None
         self.encoder = None
+        self._fit_args_count = 2
+        self._enc_fit_arg1 = ['label']
 
     def fit(self, x, y=None, **fit_params):
-        self.categ_features_inds = self._get_categorical_inds(x)
         if "enc_type" in fit_params.keys():
-            self.encoder = self.encoders[fit_params["enc_type"]]()
+            self.encoder = self._encoders[fit_params["enc_type"]]()
+        
+        if fit_params["enc_type"] in self._enc_fit_arg1:
+            self._fit_args_count = 1
         
         del fit_params["enc_type"]
 
         if self.encoder is not None:
-            self.encoder = self.encoder.fit(x[:, self.categ_features_inds], y, **fit_params)
+            match self._fit_args_count:
+                case 1: 
+                    self.encoder = self.encoder.fit(x)
+                case 2:
+                    self.categ_features_inds = self._get_categorical_inds(x)
+                    self.encoder = self.encoder.fit(x[:, self.categ_features_inds], y, **fit_params)
+
         return self
     
     def transform(self, x):
         if self.encoder is None:
             return x
-
-        encoded = self.encoder.transform(x[:, self.categ_features_inds]).toarray()
-        x = np.delete(x, self.categ_features_inds, 1)
-        x = np.concatenate((x, encoded), axis=1)
+        match self._fit_args_count:
+            case 1:
+                x = self.encoder.transform(x)
+            case 2:
+                encoded = self.encoder.transform(x[:, self.categ_features_inds]).toarray()
+                x = np.delete(x, self.categ_features_inds, 1)
+                x = np.concatenate((x, encoded), axis=1)
         return x
+    
+    def get_enc_types(self):
+        return list(self._encoders.keys())
 
     def _get_categorical_inds(self, data):
         categorical_features = list()
@@ -46,7 +61,7 @@ class Encoder(BaseEstimator, TransformerMixin):
 
 class Normalizer(BaseEstimator, TransformerMixin):
     def __init__(self, feature_rate=0.2):
-        self.normalizers = {"minmax": MinMaxScaler}
+        self._normalizers = {"minmax": MinMaxScaler}
         self.feature_rate = feature_rate
         self.num_features_inds = None
         self.normalizer = None
@@ -56,7 +71,7 @@ class Normalizer(BaseEstimator, TransformerMixin):
         self.num_features_inds = self._get_numerical_inds(x)
 
         if "normalize_type" in fit_params.keys():
-            self.normalizer = self.normalizers[fit_params["normalize_type"]]()
+            self.normalizer = self._normalizers[fit_params["normalize_type"]]()
         
         del fit_params["normalize_type"]
 
@@ -71,6 +86,9 @@ class Normalizer(BaseEstimator, TransformerMixin):
         normalized = self.normalizer.transform(x[:, self.num_features_inds])
         x[:, self.num_features_inds] = normalized
         return x
+    
+    def get_normalaizer_types(self):
+        return list(self._normalizers.keys())
 
     def _get_numerical_inds(self, data):
         numerical_features_inds = list()
@@ -91,9 +109,17 @@ def main():
 
     sc = Normalizer()
     enc = Encoder()
-    result = sc.fit_transform(x, normalize_type="minmax")
-    result2 = enc.fit_transform(result, enc_type="one_hot")
-    print(pd.DataFrame(result2, dtype='float32').head())
+
+    x = sc.fit_transform(x, normalize_type="minmax")
+    x = enc.fit_transform(x, enc_type="one_hot")
+    y = enc.fit_transform(y, enc_type="label")
+
+    print('Possible encoders: ', enc.get_enc_types())
+
+    x_df = pd.DataFrame(x, dtype='float32')
+    y_df = pd.DataFrame(y,  dtype='int')
+    df = pd.concat((x_df, y_df), axis=1)
+    print(df)
 
 
 if __name__ == '__main__':
